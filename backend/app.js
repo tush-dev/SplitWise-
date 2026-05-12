4855b54d5efe8f455b5d9bd0e1700bafcfbcc14e
Tushar Panwar <panwartushar23@gmail.com>
2026-05-12 22:07:28 +0530

    Restructure to MERN stack and setup deployment configuration

--- backend/app.js
+++ backend/app.js
@@ -21,10 +21,11 @@ app.use('/api/users', usersRouter)
 app.use('/api/group', apiAuth.validateToken,gorupRouter)
 app.use('/api/expense', apiAuth.validateToken,expenseRouter)
 
+// Static files and SPA routing (if frontend is served from same server)
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
     app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
     });
    }
 
